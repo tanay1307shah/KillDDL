03a00c115768a9dc6c13c910e7e4a398e02cce08
Tanay Shah <tanayssh@usc.edu>
2018-10-15 22:30:09 -0700

    UI fixed parts

--- app/src/main/java/com/map524s1a/killddl/MainViewActivity.java
+++ app/src/main/java/com/map524s1a/killddl/MainViewActivity.java
@@ -11,8 +11,10 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainViewActivity extends AppCompatActivity {
 
@@ -21,25 +23,48 @@ public class MainViewActivity extends AppCompatActivity {
     private FloatingActionButton addBtn;
 
 
-//    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-//            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-//
-//        @Override
-//        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-//            switch (item.getItemId()) {
-//                case R.id.list:
-//                    mTextMessage.setText(R.string.title_home);
-//                    return true;
-//                case R.id.navigation_time:
-//                    mTextMessage.setText(R.string.title_dashboard);
-//                    return true;
-//                case R.id.navigation_notifications:
-//                    mTextMessage.setText(R.string.title_notifications);
-//                    return true;
-//            }
-//            return false;
-//        }
-//    };
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            FragmentManager fm = getSupportFragmentManager();
+            Fragment f = fm.findFragmentById(R.id.fragment_container);
+            FragmentTransaction ft = fm.beginTransaction();
+            switch (item.getItemId()) {
+
+                case R.id.list:
+                    if(f == null){
+                        f = new DailyFragment();
+                        ft.add(R.id.fragment_container,f);
+                        ft.commit();
+                    }
+                    else {
+                        f = new DailyFragment();
+                        ft.replace(R.id.fragment_container, f);
+                        ft.commit();
+                    }
+                    return true;
+                case R.id.notify:
+                    return true;
+                case R.id.profile:
+                    if(f == null){
+                        f = new ProfileFragment();
+                        ft.add(R.id.fragment_container,f);
+                        ft.commit();
+                    }
+                    else {
+                        f = new ProfileFragment();
+                        ft.replace(R.id.fragment_container, f);
+                        ft.commit();
+                    }
+                    return true;
+                case R.id.deadline:
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,19 +76,19 @@ public class MainViewActivity extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
 
 
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment f = fm.findFragmentById(R.id.fragment_container);
-        FragmentTransaction ft = fm.beginTransaction();
-        if(f == null){
-            f = new MonthlyFragment();
-            ft.add(R.id.fragment_container,f);
-            ft.commit();
-        }
-        else {
-            f = new MonthlyFragment();
-            ft.replace(R.id.fragment_container, f);
-            ft.commit();
-        }
+//        FragmentManager fm = getSupportFragmentManager();
+//        Fragment f = fm.findFragmentById(R.id.fragment_container);
+//        FragmentTransaction ft = fm.beginTransaction();
+//        if(f == null){
+//            f = new MonthlyFragment();
+//            ft.add(R.id.fragment_container,f);
+//            ft.commit();
+//        }
+//        else {
+//            f = new MonthlyFragment();
+//            ft.replace(R.id.fragment_container, f);
+//            ft.commit();
+//        }
 
 
         addBtn.setOnClickListener(new View.OnClickListener() {
@@ -71,6 +96,13 @@ public class MainViewActivity extends AppCompatActivity {
             public void onClick(View v) {
                 AlertDialog.Builder builder = new AlertDialog.Builder(MainViewActivity.this);
                 View mView = getLayoutInflater().inflate(R.layout.activity_create_new_event,null);
+                Button addB = mView.findViewById(R.id.add_event);
+                addB.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(getApplicationContext(),"New Event Added!", Toast.LENGTH_SHORT).show();
+                    }
+                });
                 builder.setView(mView);
                 AlertDialog dialog =  builder.create();
                 dialog.show();
@@ -78,11 +110,8 @@ public class MainViewActivity extends AppCompatActivity {
 
         });
 
-
-
-
-      //  navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-      //  navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        //navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
 }
---
+++ app/src/main/java/com/map524s1a/killddl/ProfileFragment.java
@@ -0,0 +1,35 @@
+package com.map524s1a.killddl;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by tanay on 10/15/2018.
+ */
+
+public class ProfileFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View fm_layout = inflater.inflate(R.layout.activity_user_profile,container,false);
+        return fm_layout;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+}
