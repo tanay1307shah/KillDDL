329c0f67fe6dc3e767b391a4f9fb0eb82c4de3e4
Tanay Shah <tanayssh@usc.edu>
2018-10-20 16:05:54 -0700

    Added the inner adapter

--- app/src/main/java/com/map524s1a/killddl/DailyFragment.java
+++ app/src/main/java/com/map524s1a/killddl/DailyFragment.java
@@ -1,13 +1,22 @@
 package com.map524s1a.killddl;
 
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.List;
 
 /**
  * Created by tanay on 10/15/2018.
@@ -16,6 +25,8 @@ import android.widget.ListView;
 public class DailyFragment extends Fragment {
 
     private ListView lv;
+
+    private EventListAdapter adapter;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +44,23 @@ public class DailyFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+
+    }
+
+    public class  EventListAdapter extends ArrayAdapter<Event> {
+        public EventListAdapter(Context context, int resource, List<Event> objects){
+            super(context, resource, objects);
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            if(convertView == null){
+                convertView = getLayoutInflater().inflate(R.layout.list_item,null);
+            }
+
+            return convertView;
+        }
 
     }
 }
