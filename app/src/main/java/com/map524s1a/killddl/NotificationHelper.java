66a29bc663057179355fc4001888c366d416b9e4
Kartik Mathur <kmathur@usc.edu>
2018-10-22 10:35:02 -0700

    Working notification

---
+++ app/src/main/java/com/map524s1a/killddl/NotificationHelper.java
@@ -0,0 +1,47 @@
+package com.map524s1a.killddl;
+
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.graphics.Color;
+
+public class NotificationHelper extends ContextWrapper {
+    private static final String CHANNEL_ID = "id";
+    private static final String CHANNEL_Name = "name";
+    private NotificationManager manager;
+    public NotificationHelper(Context base){
+        super(base);
+        createChannels();
+    }
+
+    public void createChannels(){
+        //Importance_default
+        NotificationChannel channel = new NotificationChannel(CHANNEL_ID,CHANNEL_Name,NotificationManager.IMPORTANCE_DEFAULT);
+        channel.enableLights(true);
+        channel.enableVibration(true);
+        channel.setLightColor(Color.GREEN);
+        channel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+
+        getManager().createNotificationChannel(channel);
+
+    }
+
+    public NotificationManager getManager(){
+        if(manager==null)
+        {
+            manager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        }
+        return manager;
+    }
+
+    public Notification.Builder getChannelNotification(){
+        return new Notification.Builder(getApplicationContext(),CHANNEL_ID )
+                .setContentText("body")
+                .setContentTitle("title")
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setAutoCancel(true);
+    }
+}
--- app/src/main/java/com/map524s1a/killddl/ProfileFragment.java
+++ app/src/main/java/com/map524s1a/killddl/ProfileFragment.java
@@ -1,18 +1,25 @@
 package com.map524s1a.killddl;
 
 
+import android.app.Notification;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.util.Random;
 
 /**
  * Created by tanay on 10/15/2018.
  */
 
 public class ProfileFragment extends Fragment {
+
+    NotificationHelper helper;
+    Button btnSend;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -22,6 +29,7 @@ public class ProfileFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
         View fm_layout = inflater.inflate(R.layout.activity_user_profile,container,false);
         return fm_layout;
     }
@@ -29,6 +37,20 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+
+        helper = new NotificationHelper(getContext());
+        btnSend = (Button)view.findViewById(R.id.button3);
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Notification.Builder builder = helper.getChannelNotification();
+                helper.getManager().notify(new Random().nextInt(),builder.build());
+            }
+        });
+
+
 
 
     }
